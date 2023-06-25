07a717c72222ae0f89a2a60f8ff4aa5091c36ed1
admin <a>
2023-06-25 13:46:15 +0530

    Updated data_ingestion and data_transformation

---
+++ data_ingestion.py
@@ -0,0 +1,57 @@
+import os
+import sys
+from exception import CustomException
+from logger import logging
+import pandas as pd
+from dataclasses import dataclass
+import mysql.connector
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+
+
+def connect_database():
+    logging.info("Connecting to a Database")
+    try:
+        mydb = mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                passwd = "*********",# Give your database password
+                database = "stores_sales_prediction",
+                auth_plugin='mysql_native_password')
+        logging.info("Successfully connected")
+        return mydb
+    except Exception as e:
+        raise CustomException(e, sys)
+        print(e)
+
+
+def read_train_data_from_db(db):
+    ingestion_config = DataIngestionConfig()
+    logging.info("Reading the Train data from the database")
+    try:
+        df = pd.read_sql("SELECT * FROM train", con=db)
+        os.makedirs(os.path.dirname(ingestion_config.train_data_path), exist_ok=True)
+        df.to_csv(ingestion_config.train_data_path, index=False, header=True)
+        logging.info("Reading the Train data successful")
+        return ingestion_config.train_data_path
+    except Exception as e:
+        raise CustomException(e, sys)
+        print(e)
+
+
+def read_test_data_from_db(db):
+    ingestion_config = DataIngestionConfig()
+    logging.info("Reading the test data from the database")
+    try:
+        df = pd.read_sql("SELECT * FROM test", con=db)
+        os.makedirs(os.path.dirname(ingestion_config.train_data_path), exist_ok=True)
+        df.to_csv(ingestion_config.test_data_path, index=False, header=True)
+        logging.info("Reading the test data successful")
+        return ingestion_config.test_data_path
+    except Exception as e:
+        raise CustomException(e, sys)
+        print(e)
+        
--- logger.py
+++ logger.py
@@ -2,12 +2,6 @@ import logging
 import os
 from datetime import datetime
 
-# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-# logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-# os.makedirs(logs_path,exist_ok=True)
-
-# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
-
 BASE_DIR = r'F:\DOCUMENTS\Project\STORES_SALES_PREDICTION'
 
 CURRENT_TIME_STAMP=  f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
@@ -25,6 +19,3 @@ logging.basicConfig(
     level=logging.INFO,
 
 )
-
-if __name__ == "__main__":
-    logging.info("Logger started")
